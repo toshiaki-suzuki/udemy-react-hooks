e3958aecd09a703dd09cd4ab550e91baf49fd570
akis <flaflabell@gmail.com>
2022-03-04 18:46:47 +0900

    set conditions of disabled buttons

--- src/components/App.js
+++ src/components/App.js
@@ -26,7 +26,7 @@ const App = () => {
       <form>
         <div className="form-group">
           <label htmlFor="formEventTitle">タイトル</label>
-          <input className="form-control" id="formEventTitle" value={ title } onChange={ e => setTitle(e.target.value)}/>
+          <input className="form-control" id="formEventTitle" value={ title } onChange={ e => setTitle(e.target.value) }/>
         </div>
 
         <div className="form-group">
@@ -34,8 +34,8 @@ const App = () => {
           <textarea className="form-control" id="formEventBody" value={ body } onChange={ e => setBody(e.target.value)} />
         </div>
 
-        <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
-        <button className="btn btn-danger">全てのイベントを削除する</button>
+        <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
+        <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.length===0}>全てのイベントを削除する</button>
       </form>
 
       <h4>イベント一覧</h4>
