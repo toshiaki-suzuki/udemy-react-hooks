8088acf691df2ee04cdec84abd9062a5dd6620dd
akis <flaflabell@gmail.com>
2022-03-07 18:42:23 +0900

    refactor events with reducers

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,10 @@ import AppContext from "../contexts/AppContext"
 import reducer from '../reducers'
 
 const App = () => {
-
-  const [state, dispatch] = useReducer(reducer, [])
+  const initialState = {
+    events: []
+  }
+  const [state, dispatch] = useReducer(reducer, initialState)
   const value = {state, dispatch}
   return (
     <AppContext.Provider value={value}>
